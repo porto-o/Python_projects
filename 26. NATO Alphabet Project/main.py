2d43e30f050489eedfe948996970c51c94c797e8
porto-o <ismael.porto2003@gmail.com>
2023-02-17 16:00:00 -0600

    Exceptions to KeyError type added

--- 26. NATO Alphabet Project/main.py
+++ 26. NATO Alphabet Project/main.py
@@ -9,11 +9,21 @@ data = pandas.read_csv("./nato_phonetic_alphabet.csv")
 # iterrows() function is panda's function that loop over a dataframe
 nato_alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
 
-user_word = input("Enter a word: ").upper()
+
+def get_word():
+    user_word = input("Enter a word: ").upper()
+    try:
+        nato_help = [nato_alphabet[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry, only letters in the english alphabet please.")
+        get_word()
+    else:
+        print(nato_help)
+
 
 # Create a list of the phonetic code word from nato_alphabet
 # Using list comprehension, each added item is nato_alphabet value where the
 # key is the same as the letter x in the user_word
-nato_help = [nato_alphabet[letter] for letter in user_word]
 
-print(nato_help)
+
+get_word()
